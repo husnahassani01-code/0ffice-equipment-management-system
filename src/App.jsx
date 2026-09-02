0107901c4ca1f17a19a00b1453ae0dc053bbb8bb
lydiabutiku5 <lidyabutiku5@gmail.com>
2026-09-02 12:39:03 +0300

    Reconstruct project folder tree

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,91 @@
-import { useState } from 'react';
-import Sidebar from './components/sidebar'
-import Main from './components/main'
-import Header from './components/header'
-import Stock from './components/stock'
+import { useState } from "react";
 
-import './App.css'
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+
+import Dashboard from "./pages/Dashboard";
+import Receiving from "./pages/Receiving";
+import Issuing from "./pages/Issuing";
+import Returns from "./pages/Return";
+import Departments from "./pages/Department";
+
+import "./App.css";
 
 function App() {
+
   const [sidebarOpen, setSidebarOpen] = useState(true);
-   const toggleSidebar = () => {
+
+  const [currentPage, setCurrentPage] = useState("Dashboard");
+
+  const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
 
   return (
-    <div className= {`mainContainer ${sidebarOpen ? "sidebarOpen" : "sidebarClosed"}`}>
-      <Sidebar/>
+    <div
+      className={`mainContainer ${
+        sidebarOpen ? "sidebarOpen" : "sidebarClosed"
+      }`}
+    >
+
+      <Sidebar setCurrentPage={setCurrentPage} />
+
       <Header onMenuClick={toggleSidebar} />
-      <Main/>
-      <Stock/>
-      
+
+      <div className="pageArea">
+
+        {currentPage === "Dashboard" && <Dashboard />}
+
+        {currentPage === "Receiving" && <Receiving />}
+
+        {currentPage === "Issuing" && <Issuing />}
+
+        {currentPage === "Returns" && <Returns />}
+
+        {currentPage === "Departments" && <Departments />}
+
+      </div>
+
     </div>
-  )
+  );
 }
 
+export default App;
 
-export default App
+
+
+
+
+
+
+
+
+
+// import { useState } from 'react';
+// import Sidebar from './components/sidebar'
+// import Main from './components/main'
+// import Header from './components/header'
+// import Stock from './components/stock'
+// import Receiving from './pages/Receiving'
+
+// import './App.css'
+
+// function App() {
+//   const [sidebarOpen, setSidebarOpen] = useState(true);
+//    const toggleSidebar = () => {
+//     setSidebarOpen(!sidebarOpen);
+//   };
+
+//   return (
+//     <div className= {`mainContainer ${sidebarOpen ? "sidebarOpen" : "sidebarClosed"}`}>
+//       <Sidebar/>
+//       <Header onMenuClick={toggleSidebar} />
+//       <Main/>
+//       <Stock/>
+//       <Receiving/>
+//     </div>
+//   )
+// }
+
+
+// export default App
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,25 +1,73 @@
-import {BsJustify, BsPersonCircle} from 'react-icons/bs'
+import {
+  BsJustify,
+  BsPersonCircle
+} from "react-icons/bs";
 
-
-function header({ onMenuClick }) {
+function Header({ onMenuClick }) {
 
   return (
-    <div className='head'>
+    <div className="head">
+
       <div className="headContent">
-        <div className="menu-button" onClick={onMenuClick}>
-         <BsJustify className="icon" />
+
+        <div
+          className="menu-button"
+          onClick={onMenuClick}
+        >
+          <BsJustify className="icon" />
         </div>
-        <div class="search">
-          <input id="inpt" type="text" placeholder='Search here...' />
+
+        <div className="search">
+
+          <input
+            id="inpt"
+            type="text"
+            placeholder="Search here..."
+          />
+
         </div>
-        <div className='admin'>
-          <BsPersonCircle className='icon'></BsPersonCircle>
+
+        <div className="admin">
+
+          <BsPersonCircle className="icon" />
+
           <p>Admin</p>
+
         </div>
+
       </div>
-      
+
     </div>
-  )
+  );
 }
 
-export default header
+export default Header;
+
+
+
+
+// import {BsJustify, BsPersonCircle} from 'react-icons/bs'
+
+
+// function header({ onMenuClick }) {
+
+//   return (
+//     <div className='head'>
+//       <div className="headContent">
+//         <div className="menu-button" onClick={onMenuClick}>
+//          <BsJustify className="icon" />
+//         </div>
+//         <div class="search">
+//           <input id="inpt" type="text" placeholder='Search here...' />
+//         </div>
+//         <div className='admin'>
+//           <BsPersonCircle className='icon'></BsPersonCircle>
+//           <p>Admin</p>
+//         </div>
+//       </div>
+      
+//     </div>
+//   )
+// }
+
+// export default header
