6c0ab387c9823e67d40ceb941ba3a782d305bd0d
lydiabutiku5 <lidyabutiku5@gmail.com>
2026-09-02 17:02:18 +0300

    add receive, issue, department, return

---
+++ src/data/equipmentData.jsx
@@ -0,0 +1,32 @@
+const equipmentData = [
+  {
+    id: "LT-1001",
+    name: "Laptop",
+    category: "Computer",
+    quantity: 10,
+    status: "Available"
+  },
+  {
+    id: "MN-2001",
+    name: "Monitor",
+    category: "Computer",
+    quantity: 15,
+    status: "Available"
+  },
+  {
+    id: "PR-3001",
+    name: "Printer",
+    category: "Printer",
+    quantity: 5,
+    status: "Available"
+  },
+  {
+    id: "MS-4001",
+    name: "Mouse",
+    category: "Computer Accessories",
+    quantity: 30,
+    status: "Available"
+  }
+];
+
+export default equipmentData;
